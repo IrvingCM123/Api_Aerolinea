12d72249c012f9c2b4211700ff10dc05283f6921
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-08 22:51:16 -0600

    registro dto

--- src/auth/dto/registro.dto.ts
+++ src/auth/dto/registro.dto.ts
@@ -1,12 +1,13 @@
-
 import { Transform } from 'class-transformer';
 import {
-    IsEmail,
     IsNotEmpty,
     IsString,
+    Matches,
     MaxLength,
     MinLength,
-    Matches,
+    IsNumber,
+    Max,
+    Min
 } from 'class-validator';
 
 import { Error_Registro } from 'src/common/helpers/registro.helpers';
@@ -14,31 +15,39 @@ import { Error_Registro } from 'src/common/helpers/registro.helpers';
 export class RegisterDto {
 
     @IsNotEmpty()
-    @IsEmail()
-    email : string;
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) //Formato de llenado de correo: Irving.Conde123@gmail.com
+    @Matches(/^[A-Z0-9]{3}-[A-Z0-9]{3}-[A-Z0-9]{3}$/) //Formato de llenado de identificador: ABC-123-ABC
+    identificador: string;
 
     @IsNotEmpty()
     @IsString()
     @MinLength(8)
     @MaxLength(20)
     @Transform(({ value }) => value.trim())
-    password : string;
+    contraseña : string;
 
     @IsString()
-    name: string;
+    rol : string;
+
+    
+    @IsString()
+    @MaxLength(50)
+    usuario_Nombres: string;
 
     @IsString()
-    lastname: string;
+    @MaxLength(50)
+    usuario_Apellidos: string;
+
+    @IsNumber()
+    @MaxLength(3)
+    @Max(120, { message: Error_Registro.EDAD_MAXIMA })
+    @Min(18, { message: Error_Registro.EDAD_MINIMA })
+    usuario_Edad: number;
 
     @Matches(/^(\d{3})-(\d{3})-(\d{4})$/, {
         message: Error_Registro.FORMATO_TELEFONO,
     })
     @MaxLength(12)
-    phone: string;
+    Usuario_Telefono: string;
 
-    @IsString()
-    address: string;
-
-    @IsString()
-    token_notificacion?: string;
 }
