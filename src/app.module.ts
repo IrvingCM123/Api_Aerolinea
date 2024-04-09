7e4784cd2af4642ca5aee07c2bf349e387ed16a7
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-08 22:48:36 -0600

    enlace a bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,25 +4,42 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { CuentasModule } from './resource/cuentas/cuentas.module';
+import { UsuarioModule } from './resource/usuario/usuario.module';
+import * as dotenv from 'dotenv';
+
+const dotenv_Config = require('dotenv').config();
+const host = dotenv_Config.parsed.PG_HOST;
+const database = dotenv_Config.parsed.PG_DATABASE;
+const user = dotenv_Config.parsed.PG_USER;
+const password = dotenv_Config.parsed.PG_PASSWORD;
+const end_Point_Develop = dotenv_Config.parsed.ENDPOINT_ID_DEVELOP;
+const end_Point_Production = dotenv_Config.parsed.ENDPOINT_ID_PRODUCTION;
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'ep-mute-grass-a5yv0kt7-pooler.us-east-2.aws.neon.tech',
+      host: host,
       port: 5432,
-      username: 'neondb_owner',
-      password: 'I8l0nUFHumXy',
-      database: 'db_crud',
+      username: user,
+      password: password,
+      database: database,
       autoLoadEntities: true,
       synchronize: true,
       extra: {
         ssl: true,
         sslmode: 'require',
+        connection: {
+          options: `project=${end_Point_Develop}`,
+        },
       },
+      
     }),
-    AuthModule],
+    AuthModule,
+    CuentasModule,
+    UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
