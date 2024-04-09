d5cb564070e272e1b002732712612d6be062ba69
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-08 22:04:16 -0600

    enlace de tablas

--- src/resource/usuario/entities/usuario.entity.ts
+++ src/resource/usuario/entities/usuario.entity.ts
@@ -1,14 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
-import { IsString, MaxLength, IsNumber, Min, Max, Matches } from 'class-validator';
 import { Error_Registro } from 'src/common/helpers/registro.helpers';
 import { Cuenta } from 'src/resource/cuentas/entities/cuenta.entity';
-
-// Decorador personalizado para validar el formato del teléfono
-function IsTelefonoValido() {
-    return Matches(/^(\d{3})-(\d{3})-(\d{4})$/, {
-        message: Error_Registro.FORMATO_TELEFONO,
-    });
-}
 
 @Entity()
 export class Usuario {
@@ -17,25 +9,15 @@ export class Usuario {
     id_usuario: number;
 
     @Column({ nullable: false })
-    @IsString()
-    @MaxLength(50)
     usuario_Nombre: string;
 
     @Column({ nullable: false })
-    @IsString()
-    @MaxLength(50)
     usuario_Apellidos: string;
 
     @Column({ nullable: false })
-    @IsNumber()
-    @Max(120)
-    @Min(18)
     usuario_Edad: number;
 
     @Column({ nullable: false })
-    @IsString()
-    @MaxLength(12)
-    @IsTelefonoValido()
     usuario_Telefono: string;
 
     @Column() 
