6732757f2ff59db3a41d951f51493e400d088037
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-09 19:11:16 -0600

    comentar decoradores auth

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,6 +1,8 @@
 import { Roles as Rol } from './../../common/enums/roles.enum';
-import { SetMetadata } from "@nestjs/common";
+import { SetMetadata } from "@nestjs/common"; 
 
+// Define una constante 'Roles_Key' que sirve como clave para los metadatos de roles
 export const Roles_Key = 'roles';
-export const Roles = (rol: Rol) => SetMetadata(Roles_Key, rol);
 
+// Define el decorador 'Roles' que toma un rol como argumento y establece metadatos utilizando la función 'SetMetadata'
+export const Roles = (rol: Rol) => SetMetadata(Roles_Key, rol);
