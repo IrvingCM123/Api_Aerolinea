0259f3e7fc53809f8fd7787375fa5726fd8f64a3
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-12 20:47:23 -0600

    cambios

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -9,21 +9,3 @@ export function Auth(roles: Rol) {
   // Aplica los decoradores 'Roles' y 'UseGuards' al decorador que se está creando
   return applyDecorators(Roles(roles), UseGuards(AuthGuard, RoleGuard));
 }
-
-// Esta función crea un decorador de autorización que puede aceptar múltiples roles
-export function AuthAll(roles: any[]) {
-  // Inicializa un array para almacenar los decoradores
-  const decorators = [];
-
-  // Itera sobre cada rol en el array de roles proporcionado
-  for (const role of roles) {
-      // Agrega el decorador 'Roles' y el guardia 'UseGuards' para cada rol
-      decorators.push(
-          Roles(role), // Asumiendo que 'Roles' es una función decoradora
-          UseGuards(AuthGuard, RoleGuard) // Asumiendo que 'AuthGuard' y 'RoleGuard' son guardias
-      );
-  }
-
-  // Aplica todos los decoradores creados al decorador final
-  return applyDecorators(...decorators);
-}
