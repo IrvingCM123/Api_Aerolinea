faf913fe6487c7b8c28c1e760f5d6abd85844d06
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-14 19:07:30 -0600

    entity usuario_admin

--- src/resource/usuario_admin/entities/usuario_admin.entity.ts
+++ src/resource/usuario_admin/entities/usuario_admin.entity.ts
@@ -1,1 +1,15 @@
-export class UsuarioAdmin {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UsuarioAdmin {
+
+    @PrimaryGeneratedColumn()
+    id_usuarioAdmin: number;
+
+    @Column({ nullable: false, length: 50})
+    usuarioAd_Nombre: string;
+
+    @Column({ nullable: false, length: 50})
+    usuarioAd_Apellidos: string;
+
+}
