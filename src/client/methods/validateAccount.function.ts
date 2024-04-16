bfe7e43ed33da1e52924440ea78f937c2f5a4dfa
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-15 19:39:36 -0600

    cambios

--- src/client/methods/validateAccount.function.ts
+++ src/client/methods/validateAccount.function.ts
@@ -1,14 +1,14 @@
 import { email_Validate_Account_Template } from "../template/emailValidateAccount.template";
 
-function generateRandomNumber(): string {
+function generaNumeroRandom(): string {
     const randomNumber = Math.floor(100 + Math.random() * 900); // Genera un número aleatorio de 100 a 999
     return randomNumber.toString(); // Convierte el número a cadena
 }
 
 function generateRandomAccountNumber(): string {
-    const firstPart = generateRandomNumber(); // Genera la primera parte del número
-    const secondPart = generateRandomNumber(); // Genera la segunda parte del número
-    const thirdPart = generateRandomNumber(); // Genera la tercera parte del número
+    const firstPart = generaNumeroRandom(); // Genera la primera parte del número
+    const secondPart = generaNumeroRandom(); // Genera la segunda parte del número
+    const thirdPart = generaNumeroRandom(); // Genera la tercera parte del número
 
     return `${firstPart}-${secondPart}-${thirdPart}`; // Retorna el número aleatorio con el formato especificado
 }
@@ -18,4 +18,8 @@ export function validateAccount(usuario_Email: string) {
     // Quizá guardar el número generado en la bd para validar que sea el mismo, por lo mientras solo se imprime en consola y se retorna al usuario
     let template_email = email_Validate_Account_Template(usuario_Email, accountNumber); // Genera el template del email
     return { template_email, accountNumber} ;
+}
+
+export function actualizar_Contraseña() {
+
 }
