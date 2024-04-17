d0e4f72bcf688bbd79f48acd071b078386a01a00
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-04-17 08:25:53 -0600

    cambios

--- src/resource/tarjeta/tarjeta.service.ts
+++ src/resource/tarjeta/tarjeta.service.ts
@@ -1,23 +1,71 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTarjetaDto } from './dto/create-tarjeta.dto';
 import { UpdateTarjetaDto } from './dto/update-tarjeta.dto';
-
+import { TransaccionService } from 'src/common/transaction/transaccion.service';
+import { Tipo_Transaccion } from 'src/common/enums/tipo_Transaccion.enum';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tarjeta } from './entities/tarjeta.entity';
+import { Repository } from 'typeorm';
+import { Error_Tarjeta, Exito_Tarjetas } from 'src/common/helpers/tarjetas.helpers';
 @Injectable()
 export class TarjetaService {
-  create(createTarjetaDto: CreateTarjetaDto) {
-    return 'This action adds a new tarjeta';
+
+  constructor(
+    private transaccionService: TransaccionService,
+    @InjectRepository(Tarjeta)
+    private tarjetaRepository: Repository<Tarjeta>
+  ) {}
+
+  async create(createTarjetaDto: CreateTarjetaDto) {
+    let resultado = await this.transaccionService.transaction(Tipo_Transaccion.Guardar, Tarjeta, createTarjetaDto);
+
+    if (resultado != 'Éxito') {
+      return {
+        status: 400,
+        message: Error_Tarjeta.ERROR_CREAR_TARJETA
+      }
+    } else {
+      return {
+        status: 201,
+        message: Exito_Tarjetas
+      }
+    }
   }
 
   findAll() {
-    return `This action returns all tarjeta`;
+    return this.tarjetaRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tarjeta`;
+  async findOne(id: number) {
+    let buscar = await this.tarjetaRepository.findOneById(id);
+
+    if (buscar == undefined || buscar == null) {
+      return {
+        status: 400,
+        message: Error_Tarjeta.ERROR_TARJETA_NO_ENCONTRADA
+      }
+    } else {
+      return {
+        status: 201, 
+        buscar
+      }
+    }
   }
 
-  update(id: number, updateTarjetaDto: UpdateTarjetaDto) {
-    return `This action updates a #${id} tarjeta`;
+  async update(id: number, updateTarjetaDto: UpdateTarjetaDto) {
+    let resultado = await this.transaccionService.transaction(Tipo_Transaccion.Actualizar, Tarjeta, updateTarjetaDto);
+
+    if (resultado == 'Éxito') {
+      return {
+        status: 201,
+        message: Exito_Tarjetas.ACTUALIZAR_TARJETA
+      }
+    } else {
+      return {
+        status: 400,
+        message: Error_Tarjeta.ERROR_TARJETA_NO_ACTUALIZADA
+      }
+    }
   }
 
   remove(id: number) {
